0ee9960d300546c0b2b921e2e91c7c22394b9ead
seul lee <blacksangi14@naver.com>
2017-01-21 22:29:48 +0900

    no message

--- 1016.py
+++ 1016.py
@@ -1,10 +1,29 @@
 # coding: utf-8
 
+from math import sqrt, ceil
+
+
+def find_primes(num):
+    numbers = [0 for _ in range(num+1)]
+    for i, e in enumerate(numbers):
+        if i < 2:
+            continue
+        if e == 0:
+            n = 2
+            while i*n <= num:
+                numbers[i*n] = 1
+                n += 1
+    primes = [i for i in range(2, num+1) if numbers[i]]
+    return primes 
+
+
 mi, ma = map(int, input().split())
 
-numbers = [0 for _ in range(ma+1)]
+length = ma - mi + 1
+numbers = [0 for _ in range(length)]
 
-# 큰수 작은 수 빼고 더하고.
+primes = find_primes(ma)
+
 i = 2
 while i ** 2 <= ma:
     idx = i ** 2
