f1700df5b4182576a55979abdbc0f59a1d632a34
seul lee <blacksangi14@naver.com>
2017-02-13 22:11:53 +0900

    no message

--- 1036.py
+++ 1036.py
@@ -23,47 +23,50 @@ def c10_to_36(num):
     for i in range(length, -1, -1):
         n = 1
         v = 36 ** i
-        while n * v < num:
+        while n * v <= num:
             n += 1
         result += l[n-1]
-        num -= v * (n-1)
+        c = v * (n-1)
+        num -= c
     return result
 
-def solution(l, max_length, loop):
-    result = 0
-    temp = []
-    cnt = 0
-    not_finish = True
-    while not_finish:
+def solution(li, max_length ,count):
+    l = []
+    for e in li:
+        l.append(list(e))
+    result = []
+    while max_length > 0 and len(result) < count:
+        temp = []
         for e in l:
-            if len(e) == max_length:
-                temp.append(e.pop(0))
+            if len(e) == max_length: 
+                if e[0] not in result and e[0] not in temp:
+                    temp.append(e.pop(0))
+                else:
+                    e.pop(0)
         temp.sort()
-        for i in range(len(temp)):
-            result += 35 * (36 ** (max_length - 1))
-            cnt += 1
-            if cnt == loop:
-                not_finish = False
-                if temp:
-                    for a in temp:
-                        result += values[a] * (36 ** (max_length - 1))
-                break
+        result += temp
         max_length -= 1
+    return result[:count]
     
-    for e in l:
-        result += c36_to_10("".join(e))
-    return c10_to_36(result) 
 
 n = int(input())
 numbers = []
 leng = []
 
 for _ in range(n):
-    number = list(input())
+    number = input()
     numbers.append(number)
     leng.append(len(number))
 
 k = int(input())
 
-print(solution(numbers, max(leng), k))
+selected_numbers = solution(numbers, max(leng), k)
+result = 0
+
+for num_36 in numbers:
+    for sel in selected_numbers:
+        num_36 = num_36.replace(sel, "Z")
+    result += c36_to_10(num_36)
+
+print(c10_to_36(result))
 
