190d1872e71ef08152672920af75a9554ad8efe2
seul lee <blacksangi14@naver.com>
2017-02-15 21:52:28 +0900

    1036 드디어 풀이

--- 1036.py
+++ 1036.py
@@ -1,12 +1,9 @@
 # coding: utf-8
 
-from math import log
-
 l = [str(x) for x in range(10)]
 l += [chr(x) for x in range(65, 91)]
 
 values = {l[x]:x for x in range(36)}
-
 
 def c36_to_10(num):
     num = list(num)
@@ -16,57 +13,45 @@ def c36_to_10(num):
         result += values[num[i]] * (36 ** i)
     return result
 
+
 def c10_to_36(num):
-    length = int(log(num, 36))
-    digit = 0
+    temp = []
+    while num:
+        temp.append(num % 36)
+        num //= 36
+    
+    temp.reverse()
     result = ""
-    for i in range(length, -1, -1):
-        n = 1
-        v = 36 ** i
-        while n * v <= num:
-            n += 1
-        result += l[n-1]
-        c = v * (n-1)
-        num -= c
+    for i in temp:
+        result += l[i]
     return result
 
-def solution(li, max_length ,count):
-    l = []
-    for e in li:
-        l.append(list(e))
-    result = []
-    while max_length > 0 and len(result) < count:
-        temp = []
-        for e in l:
-            if len(e) == max_length: 
-                if e[0] not in result and e[0] not in temp:
-                    temp.append(e.pop(0))
-                else:
-                    e.pop(0)
-        temp.sort()
-        result += temp
-        max_length -= 1
-    return result[:count]
-    
 
 n = int(input())
 numbers = []
-leng = []
 
 for _ in range(n):
     number = input()
     numbers.append(number)
-    leng.append(len(number))
+
+plus = []
+result = 0
 
 k = int(input())
 
-selected_numbers = solution(numbers, max(leng), k)
-result = 0
+for number in numbers:
+    result += c36_to_10(number)
 
-for num_36 in numbers:
-    for sel in selected_numbers:
-        num_36 = num_36.replace(sel, "Z")
-    result += c36_to_10(num_36)
+for e in l:
+    temp = 0
+    for number in numbers:
+        num = number.replace(e, "Z")
+        temp += c36_to_10(num)
+    plus.append(temp - result)
 
-print(c10_to_36(result))
+plus.sort()
+plus.reverse()
+
+result = c10_to_36(result + sum(plus[:k]))
+print(result)
 
