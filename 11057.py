2f8428a14322d0d8bd761522867165fdfaa372c0
seul lee <blacksangi14@naver.com>
2017-03-18 18:10:42 +0900

    11057 수정

--- 11057.py
+++ 11057.py
@@ -10,9 +10,7 @@ for i in range(2, n+1):
     for j in range(10):
         dp[i][j] = dp[i][j-1] + dp[i-1][j]
 
-ans = 0
-for i in range(10):
-    ans += dp[n][i]
+ans = sum(dp[n])
 
 print(ans % 10007)
 
