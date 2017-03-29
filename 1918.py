34d394c731b565d7ce83909f3f60b70d6444214e
seul lee <blacksangi14@naver.com>
2017-03-29 13:55:37 +0900

    1918 리팩토링

--- 1918.py
+++ 1918.py
@@ -1,59 +1,30 @@
 # coding: utf-8
 
-def empty():
-    if len(stack) == 0:
-        return True
-    return False
 
-def top():
-    if not empty():
-        return stack[-1]
-    return -1
-
-op_list = ["-", "+", "*", "/"]
-
-def op_order(op):
-    if op == "*" or op == "/":
-        return 2
-    elif op == "+" or op == "-":
-        return 1
-    else: # op == "("
-        return 0
+op_order = {"-": 1, "+": 1, "*": 2, "/": 2, "(": 0}
 
 ex = input()
-postfix = ""
-
 stack = []
 
-index = 0
-while index < len(ex):
-    if ex[index] not in op_list and ex[index] != "(" and ex[index] != ")":
-        postfix += ex[index]
-    elif ex[index] == "(":
-        stack.append(ex[index])
-    elif ex[index] in op_list:
-        while True:
-            top_val = top()
-            if op_order(ex[index]) <= op_order(top_val):
-                postfix += stack.pop()
-            else:
-                break 
-        stack.append(ex[index])
-    elif ex[index] == ")":
-        while True:
-            top_val = top()
-            if top_val == "(":
-                stack.pop()
-                break
-            else:
-                postfix += stack.pop()
-    index += 1
+for c in ex:
+    if c in op_order.keys():
+        if not stack or c == '(':
+            stack.append(c)
+        else:
+            while stack and op_order[c] <= op_order[stack[-1]]:
+                print(stack.pop(), end='')
+            stack.append(c)
+    
+    elif c == ')':
+        while stack and stack[-1] != '(':
+            print(stack.pop(), end='')
 
-for _ in range(len(stack)):
-    postfix += stack.pop()
+        if stack and stack[-1] == '(':
+            stack.pop()
 
-print(postfix)
+    else:
+        print(c, end='')     
 
-
-
-
+while stack:
+    print(stack.pop(), end='')
+ 
