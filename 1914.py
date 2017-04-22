be82617516a61e65ec0c9b3b069e6ffe9f4f39cb
seul lee <blacksangi14@naver.com>
2017-04-22 17:09:20 +0900

    1914 하노이 탑 풀이

---
+++ 1914.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+
+def tower_of_hanoi(n, frompeg, topeg, auxpeg):
+    if n == 1:
+        print("{} {}".format(frompeg, topeg))
+        return
+    tower_of_hanoi(n-1, frompeg, auxpeg, topeg)
+    
+    print("{} {}".format(frompeg, topeg))
+    
+    tower_of_hanoi(n-1, auxpeg, topeg, frompeg)
+
+
+n = int(input())
+
+
+cnt = 2**n - 1
+print(cnt)
+
+if n <= 20:
+    tower_of_hanoi(n, 1, 3, 2)
+
