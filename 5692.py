7b0fbbf6b0257cabf9e4083bcd21d6a46d9d1a62
seul <blacksangi@gmail.com>
2018-04-17 10:25:57 +0900

    5692 문제 풀이 수정

--- 5692.py
+++ 5692.py
@@ -1,20 +1,20 @@
 # coding: utf-8
 
-def fac(num):
-    result = 1
-    for i in range(2, num+1):
-        result *= i
-    return result
+import sys
+
+
+fac = [1, 2, 6, 24, 120]
 
 while True:
-    n = int(input())
-    if not n:
-        break
+    n = sys.stdin.readline().strip()
+    if n == '0':
+        break 
+    n = list(n)
+    n.reverse()
+
     result = 0
-    n = list(str(n))
-    i = 1
-    while n:
-        result += int(n.pop()) * fac(i)
-        i += 1
-    print(result)
-        
+    for i, num in enumerate(n):
+        result += int(num) * fac[i]
+    
+    sys.stdout.write(str(result) + '\n')
+
