2e7d7dd96d84d8d0244a49ae0c73151807e580d9
seul lee <blacksangi14@naver.com>
2018-08-08 02:24:12 +0900

    5618 문제 수정

--- 5618.py
+++ 5618.py
@@ -1,16 +1,29 @@
 # coding: utf-8
 
-n = int(input())
-numbers = list(map(int, input().split()))
 
-result = 0
-for i in range(1, min(numbers) + 1):
-    for number in numbers:
-        if number % i:
-            result = 0
-            break
-        else:
-            result = 1
-    if result:
-        print(i)
+import math
 
+
+n = input()
+a = list(map(int, input().split()))
+
+g = a[0]
+gcd = lambda x,y:x if not y else gcd(y,x%y)
+
+for i in a:
+    g=gcd(g,i)
+
+x = int(math.sqrt(g))
+
+ans=[]
+for i in range(1,x+1):
+    if g % i > 0:
+        continue
+    
+    ans.append(i)
+    if i != g / i:
+        ans.append(int(g/i))
+
+ans.sort()
+for i in ans:
+    print(i)
