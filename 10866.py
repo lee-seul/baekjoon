8ce413c194b14e6d65f7c43a7ddbee7a44c0ff9f
seul lee <blacksangi14@naver.com>
2018-08-20 10:33:04 +0900

    10866 클래스로 수정

--- 10866.py
+++ 10866.py
@@ -4,56 +4,90 @@
 import sys
 
 
-def sys_print(value):
+def sys_print(value=-1):
     return sys.stdout.write(str(value)+'\n')
 
 
+class Deq:
+
+    def __init__(self):
+        self._data = []
+
+    def __len__(self):
+        return len(self._data)
+
+    def __getitem__(self, position):
+        return self._data[position]
+
+    def _insert(self, value, position):
+        self._data.insert(position, value)
+
+    def _pop(self, position):
+        return self._data.pop(position)
+
+    def _is_empty(self):
+        if not len(self):
+            return True
+        return False
+
+    def push(self, value, position=-1):
+        if not position == -1:
+            self._insert(position, value)
+            return 
+        self._data.append(value)
+
+    def pop_data(self, position=-1):
+        if self._is_empty():
+            return sys_print()
+        return sys_print(self._pop(position))
+
+    def empty(self):
+        if self._is_empty():
+            return sys_print(1)
+        return sys_print(0)
+
+    def front(self):
+        if self._is_empty():
+            return sys_print()
+        return sys_print(self[0])
+
+    def back(self):
+        if self._is_empty():
+            return sys_print()
+        return sys_print(self[-1])
+    
+    def size(self):
+        return sys_print(len(self))
 
 
 n = int(input())
 
-d = []
+d = Deq()
 for _ in range(n):
     statement = sys.stdin.readline().strip()
     statement = statement.split()
     s = statement[0]
     if s == 'push_back':
-        d.append(statement[1])
+        d.push(statement[1])
 
     if s == 'push_front':
-        d.insert(0, statement[1])
+        d.push(statement[1], 0)
 
     if s == 'pop_front':
-        if not len(d):
-            sys_print(-1)
-        else:
-            sys_print(d.pop(0))
+        d.pop_data(0)
 
     if s == 'pop_back':
-        if not len(d):
-            sys_print(-1)
-        else:
-            sys_print(d.pop())
+        d.pop_data()
 
     if s == 'size':
-        sys_print(len(d))
+        d.size()
 
     if s == 'empty':
-        if not len(d):
-            sys_print(1)
-        else:
-            sys_print(0)
+        d.empty()
 
     if s == 'front':
-        if not len(d):
-            sys_print(-1)
-        else:
-            sys_print(d[0])
+        d.front()
 
     if s == 'back':
-        if not len(d):
-            sys_print(-1)
-        else:
-            sys_print(d[-1])
-
+        d.back()
 
