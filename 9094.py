a462c60da2c9a378f2794421a00f7fde00d61fe7
seul lee <blacksangi14@naver.com>
2018-08-23 10:08:43 +0900

    9094 수정

--- 9094.py
+++ 9094.py
@@ -5,8 +5,8 @@ def count_integer(n, m):
     result = 0
     for i in range(1, n-1):
         for j in range(i+1, n):
-            value = (i*i+j*j+m)/(i*j)
-            if value == int(value):
+            value = (i*i+j*j+m)%(i*j)
+            if not value:
                 result += 1
     return result
 
